2d52046516860c74a60277db92e6014c644e54fe
Gurinderpal Singh <gurinder.narang88@gmail.com>
2020-08-23 22:40:46 +0530

    User-post relation added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,16 +2,19 @@ class PostsController < ApplicationController
 
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
+  before_action :authenticate_user!
+
   def index
     @posts = Post.all
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    if @post = Post.create(post_params)
+    @post = current_user.posts.build(post_params)
+    if @post.save
       flash[:success] = "Your Post has been created!"
       redirect_to posts_path
     else
